62832cef4612526a117fe67629bdafac927b0c1b
Joseph <jaathunder@yahoo.com>
2019-10-23 20:04:12 -0700

    add logic file for comparing || and |

---
+++ src/Week08/Logic.java
@@ -0,0 +1,18 @@
+package Week08;
+
+public class Logic {
+
+    public static void main(String[] args) {
+
+        int x = 0;
+        int y = 1;
+
+        if (x == 0 || y == 0) {
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+
+    }
+
+}
