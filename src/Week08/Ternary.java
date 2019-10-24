6ef907ca07e5993f90404debea8b816a6e459c95
Joseph <jaathunder@yahoo.com>
2019-10-23 19:35:43 -0700

    Add ternary operator for review practice

---
+++ src/Week08/Ternary.java
@@ -0,0 +1,13 @@
+package Week08;
+
+public class Ternary {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        String state = i == 5?"i = 5":"i = 0";
+        System.out.println(state);
+
+    }
+
+}
