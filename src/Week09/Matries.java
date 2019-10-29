c036871b69bb8f1057540887bd4f44cc0bfa34f6
Joseph <jaathunder@yahoo.com>
2019-10-29 09:24:16 -0700

    Add matrices file

---
+++ src/Week09/Matries.java
@@ -0,0 +1,13 @@
+package Week09;
+
+public class Matries {
+
+    public static void main(String[] args) {
+
+        double A[][] = new double[4][3];
+        double B[][] = new double[3][2];
+        double C[][] = new double[A.length][B[0].length];
+
+    }
+
+}
