947e54e73bcec6881d037cbf083c77d7fd432e92
Joseph <jaathunder@yahoo.com>
2019-11-05 09:11:59 -0800

    change name to GCtest as this is a test of trying to detect when GC is fired, but this actually no longer works

---
+++ src/Week10/GCtest.java
@@ -0,0 +1,28 @@
+package Week10;
+
+public class GCtest {
+
+    public static void main(String[] args) {
+
+        int x[]; //x is reference to int
+        for(int i = 0; i < 1000; ++i) {
+            x = new int[1000]; //heap, allocate new
+            x[0] = 0;
+
+
+        }
+
+    }
+
+    //you are already creating an object when defining a new class, so therefore this name of this function already exists
+    //defining this as an override must be correct, see the java code when doing so
+
+    //as of java 9, this NOT LONGER WORKS!!!
+    //therefore changing this does nothing
+    //it has a @depreciated in source
+    public void finalize() {
+        //garbage collection
+        System.out.println("Garbage Collection!!!");
+    }
+
+}
