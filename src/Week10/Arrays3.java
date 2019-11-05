32f19c55f3d82e039fd2417fc5b76822a2533f9c
Joseph <jaathunder@yahoo.com>
2019-11-05 08:13:15 -0800

    array3 edit2 for more info

--- src/Week10/Arrays3.java
+++ src/Week10/Arrays3.java
@@ -2,18 +2,24 @@ package Week10;
 
 public class Arrays3 {
 
+    //***java is PASS BY VALUE ONLY, which means that variables on the stack will not be sharded across scopes***
+
     public static void main(String[] args) {
 
         int y[] = {133, 0};
-        func(y);
-
+        //changes in the function call...
+        System.out.println(func(y));
     }
 
-    public static void func (int x[]) {
+    public static int func (int x[]) {
+        //...do not change here
+        x = new int[x.length];
+        //will override the reference value of the array
         try {
             x[0] = 27;
+            return x[1];
         }catch (Exception e) {
-            System.out.println("invalid");
+            return -1;
         }
     }
 
