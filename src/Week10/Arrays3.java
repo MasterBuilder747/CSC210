0664154833678c95379d0341ecf1adafb89e7712
Joseph <jaathunder@yahoo.com>
2019-11-05 08:54:26 -0800

    finish arrays3

--- src/Week10/Arrays3.java
+++ src/Week10/Arrays3.java
@@ -3,17 +3,31 @@ package Week10;
 public class Arrays3 {
 
     //***java is PASS BY VALUE ONLY, which means that variables on the stack will not be sharded across scopes***
+    //cannot modify the contents of the stack within a function
+    //in "2D arrays", the references are in a reference, in which each reference in it can be modified but not the outside reference
 
     public static void main(String[] args) {
 
-        int y[] = {133, 0};
-        //changes in the function call...
-        System.out.println(func(y));
-    }
+        int[] y; //allocates reference var on stack
+        y = new int[1]; //instantiates on the heap
+        y[0] = 13; //assigns value on heap
+        //if you have a throws statement in a function, it must be handled in a try catch block
+        //func(y);
+        try {
+            System.out.println(y[0]);
+            func(y); //creates new scope on the stack
+            System.out.println(y[0]);
+        }catch (Exception e) {
+            System.out.println(e);
+        }
 
-    public static int func (int x[]) {
+    }//stack goes away
+    //heap goes away with no references
+    //GC cleans up
+
+    public static int func (int x[]) throws Exception {
         //...do not change here
-        x = new int[x.length];
+        x = new int[x.length]; //reference gets instantiated on heap
         //will override the reference value of the array
         try {
             x[0] = 27;
