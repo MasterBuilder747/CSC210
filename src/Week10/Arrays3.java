346bc1d725a59c606d2f32cd055f765c6171c74c
Joseph <jaathunder@yahoo.com>
2019-11-05 08:01:31 -0800

    array file edit (review)

--- src/Week10/Arrays3.java
+++ src/Week10/Arrays3.java
@@ -4,8 +4,17 @@ public class Arrays3 {
 
     public static void main(String[] args) {
 
+        int y[] = {133, 0};
+        func(y);
 
+    }
 
+    public static void func (int x[]) {
+        try {
+            x[0] = 27;
+        }catch (Exception e) {
+            System.out.println("invalid");
+        }
     }
 
 }
