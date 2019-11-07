8dc045453b44ad7f4b5ebe8c72f92abb83e5da4a
Joseph <jaathunder@yahoo.com>
2019-11-07 08:31:56 -0800

    create and move largenumberhandling to another class for organization, update OOPL

---
+++ src/Week10/LargeNumberHandling.java
@@ -0,0 +1,15 @@
+package Week10;
+
+public class LargeNumberHandling {
+
+    public static void main(String[] args) {
+
+        //two really large positive numbers equals a negative number
+        int big1 = 2105169878;
+        int big2 = 2105169870;
+        System.out.println(big1 + big2);
+
+
+    }
+
+}
---
+++ src/Week10/OOPL.java
@@ -0,0 +1,16 @@
+package Week10;
+
+public class OOPL {
+
+    public static void main(String[] args) {
+        //object-oriented programming language
+
+
+        EmployeeRecord[] er = new EmployeeRecord[100];
+
+        //take everything and break it into smaller pieces
+        //object provides a service for the larger program
+
+    }
+
+}
