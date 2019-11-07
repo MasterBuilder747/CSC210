6c7ab357c984ad92394a5d5ff84241f8a773dcb0
Joseph <jaathunder@yahoo.com>
2019-11-07 09:26:53 -0800

    add and finalize all files for week 10

--- src/Week10/EmployeeRecord.java
+++ src/Week10/EmployeeRecord.java
@@ -4,9 +4,23 @@ public class EmployeeRecord {
 
     //instead of using arrays in the main method, storing all the data types in one object is much more organized
 
-    //this is an array object
-    char[] name;
+    //if you don't provide a constructor, the default will be provided for you
+    // 1) allocates heap space
+    // 2) initializes member variables
+    //  numeric types get 0
+    //  chars and strings get 0 (int type)
+    //  boolean get false
+    //  otherwise it must be a reference, which is null (hence the NullPointerException when accessed)
+    //  sout works differently and may not actually print out the right variable that is actually stored
+    String name;
     int id;
     float salary;
+    boolean working;
+
+    public EmployeeRecord() { //void works in constructor method, even though it is redundant
+        int x = 0;
+        System.out.println("construct an EmployeeRecord");
+    }
+
 
 }
--- src/Week10/OOPL.java
+++ src/Week10/OOPL.java
@@ -4,12 +4,35 @@ public class OOPL {
 
     public static void main(String[] args) {
         //object-oriented programming language
-
-
-        EmployeeRecord[] er = new EmployeeRecord[100];
-
         //take everything and break it into smaller pieces
         //object provides a service for the larger program
+
+        //***class method of procedural***
+        int N = 5;
+        EmployeeRecord[] er = new EmployeeRecord[N]; //creates an array of references to employee record classes
+        //in this case, the class does not exist yet, only the references
+        //the data is not set and stored yet
+
+        //soooo.... construct it! (always)
+
+        EmployeeRecord r = new EmployeeRecord(); //this is an individual class, invokes constructor
+
+        //store vales for the
+        for (int i = 0; i < N; ++i) {
+            er[i] = new EmployeeRecord(); //constructor
+            //this is an array of i classes, used more often
+            //er[i].name = "MyNameIs" + i; //results in a nullpointerexception because it needs to be constructed!
+            //er[i].salary = 13.0F; //13.00
+            //er[i].id = i;
+            //er[i].working = true;
+        }
+
+        //print the values
+        for(int i = 0; i < N; i++) {
+            System.out.println(er[i].name + "\t" +
+                               er[i].id + "\t" +
+                               er[i].salary);
+        }
 
     }
 
