0e811fc1fa64508b385426d1af67a928c594b237
Joseph <jaathunder@yahoo.com>
2019-11-12 09:13:41 -0800

    readd week11 commit

---
+++ src/Week11/EmployeeRecord.java
@@ -0,0 +1,16 @@
+package Week11;
+
+//using a class from another package must be imported
+//import Week10.EmployeeRecord;
+
+public class EmployeeRecord {
+
+    public static void main(String[] args) {
+
+        //EmployeeRecord = new EmployeeRecord();
+
+        //override: they are in different classes
+
+    }
+
+}
