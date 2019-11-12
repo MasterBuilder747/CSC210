4f1d7ec24e6df709deadd40ec523c3013dde953d
Joseph <jaathunder@yahoo.com>
2019-11-12 09:19:47 -0800

    update week10 file

--- src/Week10/EmployeeRecord.java
+++ src/Week10/EmployeeRecord.java
@@ -28,25 +28,22 @@ public class EmployeeRecord {
     //default access is not public, it is:
     //public within the package, private outside of the package
 
-    //constructors are usually public
-    public EmployeeRecord() { //void works in constructor method, even though it is redundant
+    //constructors: usually public, but private if needed
+    EmployeeRecord() { //void works in constructor method, even though it is redundant
         int x = 0;
         System.out.println("construct an EmployeeRecord");
         numEmploy++;
     }
-
     public EmployeeRecord(double x) {
         numEmploy++;
     }
-
     //"this" works when in the class regardless of private
     public EmployeeRecord(String name, int id) {
         this.name = name;
         this.id = id;
         numEmploy++;
     }
-
-    public EmployeeRecord(String name, int id, float salary) { //overloading: use different sets of parameters for the same constructor
+    EmployeeRecord(String name, int id, float salary) { //overloading: use different sets of parameters for the same constructor
         //parameters are retrieved from the stack, and are placed here in this scope
         //must transfer to the class scope
         //must be significant (different data types)
@@ -64,20 +61,17 @@ public class EmployeeRecord {
     //used for allowing for certain variables to be able to be set and accessed
 
     //getters
-    public String getName() {
+    String getName() {
         return name;
     }
-
-    public int getId() {
+    int getId() {
         forInternalUseOnly(); //notice that "this" is not needed here
         return id;
     }
-
-    public float getSalary() {
+    float getSalary() {
         return salary;
     }
-
-    public static int getNumEmploy() {
+    static int getNumEmploy() {
         //can be called before the constructor
         //accessible before instantiation when static
         //cannot access non-static variables in a static context:
@@ -93,25 +87,24 @@ public class EmployeeRecord {
     public void setName(String name) {
         this.name = name;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public void setSalary(float salary) {
         this.salary = salary;
     }
-
     private void forInternalUseOnly() {
         //cullable from only inside the class
     }
 
     //override
+    @Override //this is a comment, used for javadoc formatting, ignored by the compiler
     public String toString() {
         if (getName() == null) {
             return "no name" + "\t" + this.getId() + "\t" + this.getSalary();
+        }else {
+            return this.getName() + "\t" + this.getId() + "\t" + this.getSalary();
         }
-        return this.getName() + "\t" + this.getId() + "\t" + this.getSalary();
     }
     //String eString = er.[1].toString();
 }
