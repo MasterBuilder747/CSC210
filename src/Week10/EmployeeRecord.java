142d4e9627e50a44080aa43ff791da9fbbdb17de
Joseph <jaathunder@yahoo.com>
2019-11-12 09:13:05 -0800

    add week11, finish week10 files with contructors, getters/setters, and public-static explanation

--- src/Week10/EmployeeRecord.java
+++ src/Week10/EmployeeRecord.java
@@ -12,15 +12,106 @@ public class EmployeeRecord {
     //  boolean get false
     //  otherwise it must be a reference, which is null (hence the NullPointerException when accessed)
     //  sout works differently and may not actually print out the right variable that is actually stored
-    String name;
-    int id;
-    float salary;
-    boolean working;
 
+    //access modifiers:
+    // to variables and functions
+    //public, private, protected
+    //public: any class
+    //private: only the class
+    //use private for class vars to maintain control/security
+    //aka data//function hiding
+    private String name;
+    private int id;
+    private float salary;
+    static private int numEmploy; //every class has its own copy
+    //static: not like normal definition: shared across all instances of that class (good for count variables)
+    //default access is not public, it is:
+    //public within the package, private outside of the package
+
+    //constructors are usually public
     public EmployeeRecord() { //void works in constructor method, even though it is redundant
         int x = 0;
         System.out.println("construct an EmployeeRecord");
+        numEmploy++;
     }
 
+    public EmployeeRecord(double x) {
+        numEmploy++;
+    }
 
+    //"this" works when in the class regardless of private
+    public EmployeeRecord(String name, int id) {
+        this.name = name;
+        this.id = id;
+        numEmploy++;
+    }
+
+    public EmployeeRecord(String name, int id, float salary) { //overloading: use different sets of parameters for the same constructor
+        //parameters are retrieved from the stack, and are placed here in this scope
+        //must transfer to the class scope
+        //must be significant (different data types)
+
+        //"this" will bypass the local scope, using the class-level scope
+        //aka: self-reference operator
+        //use this scope
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+        numEmploy++;
+    }
+
+    //getters and setters (IDE and OOP design)
+    //used for allowing for certain variables to be able to be set and accessed
+
+    //getters
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        forInternalUseOnly(); //notice that "this" is not needed here
+        return id;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public static int getNumEmploy() {
+        //can be called before the constructor
+        //accessible before instantiation when static
+        //cannot access non-static variables in a static context:
+        /*
+        if (id == 27) {
+
+        }
+        */
+        return numEmploy;
+    }
+
+    //setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    private void forInternalUseOnly() {
+        //cullable from only inside the class
+    }
+
+    //override
+    public String toString() {
+        if (getName() == null) {
+            return "no name" + "\t" + this.getId() + "\t" + this.getSalary();
+        }
+        return this.getName() + "\t" + this.getId() + "\t" + this.getSalary();
+    }
+    //String eString = er.[1].toString();
 }
--- src/Week10/OOPL.java
+++ src/Week10/OOPL.java
@@ -2,11 +2,16 @@ package Week10;
 
 public class OOPL {
 
-    public static void main(String[] args) {
+    //all objects-can be called before instantiation-no return type-array of string arguments
+    //gives JVM access-no object exists-nothing can be returned to the JVM-name(exception arguments)
+    public static void main(String[] args) { //entry point, static, only called by the JVM
+        //this means that it doesn't know what the class is before calling the function
+
         //object-oriented programming language
         //take everything and break it into smaller pieces
         //object provides a service for the larger program
 
+        System.out.println(EmployeeRecord.getNumEmploy()); //cannot accees non-static variables/functions
         //***class method of procedural***
         int N = 5;
         EmployeeRecord[] er = new EmployeeRecord[N]; //creates an array of references to employee record classes
@@ -19,7 +24,7 @@ public class OOPL {
 
         //store vales for the
         for (int i = 0; i < N; ++i) {
-            er[i] = new EmployeeRecord(); //constructor
+            er[i] = new EmployeeRecord("MyNameIs" + i, i, 13.0F); //constructor
             //this is an array of i classes, used more often
             //er[i].name = "MyNameIs" + i; //results in a nullpointerexception because it needs to be constructed!
             //er[i].salary = 13.0F; //13.00
@@ -29,9 +34,9 @@ public class OOPL {
 
         //print the values
         for(int i = 0; i < N; i++) {
-            System.out.println(er[i].name + "\t" +
-                               er[i].id + "\t" +
-                               er[i].salary);
+            System.out.println(er[i].getName() + "\t" +
+                               er[i].getId() + "\t" +
+                               er[i].getSalary());
         }
 
     }
