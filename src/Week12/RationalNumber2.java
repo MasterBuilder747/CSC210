9fa905ad1966e1b22a96ee3931c02c8cc34ccfd1
Joseph <jaathunder@yahoo.com>
2019-11-21 09:19:38 -0800

    add euclid's algorithm method and a flip method

--- src/Week12/RationalNumber2.java
+++ src/Week12/RationalNumber2.java
@@ -67,52 +67,40 @@ public class RationalNumber2 {
         Scanner kb = new Scanner(System.in);
 
         try {
-            int num = kb.nextInt();
+            /*
             System.out.println("numerator: ");
-            int den = kb.nextInt();
+            int num = kb.nextInt();
             System.out.println("denominator: ");
+            int den = kb.nextInt();
+
+             */
 
             RationalNumber2 r0 = new RationalNumber2(1, 2);
-            RationalNumber2 r1 = new RationalNumber2(3, 4);
+            RationalNumber2 r1 = new RationalNumber2(3, 2);
+            System.out.println("r0: " + r0);
+            System.out.println("r1: " + r1);
             System.out.println("add: " + r0.add(r1));
-            System.out.println("sub: " + r0.sub(r1));
-            System.out.println("mul: " + r0.mul(r1));
-            System.out.println("div: " + r0.div(r1));
-
+            //System.out.println("sub: " + r0.sub(r1));
+            //System.out.println("mul: " + r0.mul(r1));
+            //System.out.println("div: " + r0.div(r1));
         }catch (IllegalArgumentException e) {
             System.out.println(e);
         }
-        System.out.println("default: " + new RationalNumber2());
+        //System.out.println("default: " + new RationalNumber2());
         kb.close();
-    }
-
-    public int getNumerator() {
-        return numerator;
-    }
-
-    public void setNumerator(int numerator) {
-        this.numerator = numerator;
-    }
-
-    public int getDenominator() {
-        return denominator;
-    }
-
-    public void setDenominator(int denominator) {
-        this.denominator = denominator;
-    }
-
-    public int getSign() {
-        return sign;
-    }
-
-    public void setSign(int sign) {
-        this.sign = sign;
     }
 
     // -- this + rhs
     public RationalNumber2 add(RationalNumber2 rhs) {
-        return new RationalNumber2();
+        int n = 0;
+        int d = 0;
+        if (this.denominator == rhs.denominator) {
+            n = this.numerator + rhs.numerator;
+            d = rhs.denominator;
+        }else{
+
+        }
+        return new RationalNumber2(n, d);
     }
 
     // -- this - rhs
@@ -128,6 +116,43 @@ public class RationalNumber2 {
     // -- this / rhs
     public RationalNumber2 mul(RationalNumber2 rhs) {
         return new RationalNumber2();
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+    public int getDenominator() {
+        return denominator;
+    }
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+    public int getSign() {
+        return sign;
+    }
+    public void setSign(int sign) {
+        this.sign = sign;
+    }
+
+    //make private for the secret euclid algorithm
+    private int gcd(int a, int b) {
+        int dividend = a >= b ? a : b;
+        int divisor = a <= b ? a : b;
+        while(divisor != 0) {
+            int remainder = dividend % divisor;
+            dividend = divisor;
+            divisor = remainder;
+        }
+        return dividend;
+    }
+
+    private RationalNumber2 flip() {
+        int a = this.denominator;
+        int b = this.numerator;
+        return new RationalNumber2(a, b);
     }
 
 }
